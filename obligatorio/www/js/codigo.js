cfdebdbbb7663d4f8f95f9760df1aeab374e53ff
Nicolás Urraburu <106207057+balurra@users.noreply.github.com>
2023-02-15 10:50:58 -0300

    Add files via upload

--- obligatorio/www/js/codigo.js
+++ obligatorio/www/js/codigo.js
@@ -9,18 +9,28 @@ function Eventos() {
   document.querySelector("#btnRegistrar").addEventListener("click", TomarDatosRegistro);
   document.querySelector("#slcDeptos").addEventListener('ionChange',setearDepto);
   document.querySelector("#slcCiudades").addEventListener('ionChange',setearCiudad);
+  document.querySelector("#btnLogin").addEventListener("click", TomarDatosLogin);
+
 
 }
 
 function ArmarMenu() {
-  let hayToken = localStorage.getItem("apikey");
+  let hayToken = localStorage.getItem("apiKey");
   if (!hayToken) {
   dqs("menu-opciones").innerHTML = `  <ion-item href="/" onclick="cerrarMenu()">HOME</ion-item>
   <ion-item href="/login" onclick="cerrarMenu()">LOGIN</ion-item>
   <ion-item href="/registro" onclick="cerrarMenu()">REGISTRAR</ion-item>`
   }else{
-    dqs("menu-opciones").innerHTML = `  <ion-item href="/" onclick="cerrarMenu()">HOME</ion-item>`
+    dqs("menu-opciones").innerHTML = `  <ion-item href="/" onclick="cerrarMenu()">HOME</ion-item>
+    <ion-item href="/gasto" onclick="cerrarMenu()">AGREGAR GASTO</ion-item>
+    <ion-item href="/logout" onclick="cerrarMenu()">LOGOUT</ion-item>`
   }
+}
+
+function CerrarSesion(){
+  localStorage.clear();
+  NAV.push("page-home");
+  ArmarMenu();
 }
 
 function setearDepto(evt){
@@ -31,11 +41,43 @@ function setearCiudad(evt){
   console.log(evt);
 }
 
+function TomarDatosLogin(){
+  let u = dqs("txtLoginUsuario").value;
+  let p = dqs("txtLoginPass").value;
+  let usuLog = new LoginDTO(u,p);
+  console.log(usuLog);
+
+  fetch(`${URLBASE}login.php`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(usuLog),
+  })
+    .then(function (response) {
+      console.log(response);
+      if (response.status == 200) {
+        return response.json();
+      }else{
+         throw response;
+      }
+    })
+    .then(function (dato) {
+        localStorage.setItem("apiKey", dato.apiKey);
+        dqs("pLogin").innerHTML = "Login exitoso!";
+        ArmarMenu();
+    })
+
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
 function TomarDatosRegistro() {
     let u = dqs("txtRegistroUsuario").value;
     let p = dqs("txtRegistroPass").value;
-    let d = dqs("txtRegistroDepto").value;
-    let c = dqs("txtRegistroCiudad").value;
+    let d = Number(dqs("slcDeptos").value);
+    let c = Number(dqs("slcCiudades").value);
   
     let usuario = new Usuario(u, p, d, c);
   
@@ -57,7 +99,9 @@ function TomarDatosRegistro() {
         }
       })
       .then(function (dato) {
-          dqs("pRegistroRes").innerHTML = "Registro exitoso";
+        localStorage.setItem("apiKey", dato.apiKey);
+        dqs("pRegistroRes").innerHTML = "Registro exitoso!";
+        ArmarMenu();
       })
   
       .catch(function (error) {
@@ -82,8 +126,9 @@ function TomarDatosRegistro() {
       REGISTRO.style.display = "block";
       RecargarDeptos();
       RecargarCiudades();
-
-    }
+    } else if (ruta == "/logout") {
+     CerrarSesion();
+    } 
   }
 
 
--- obligatorio/www/js/estructuras.js
+++ obligatorio/www/js/estructuras.js
@@ -3,16 +3,16 @@ let UsuarioLogueado = false;
 
 class Usuario {
     constructor(user, password, depto, ciudad) {
-      this.user = user;
+      this.usuario = user;
       this.password = password;
-      this.depto = depto;
-      this.ciudad = ciudad;
+      this.idDepartamento = depto;
+      this.idCiudad = ciudad;
     }
   }
 
 class LoginDTO {
     constructor(user, password) {
-      this.user = user;
+      this.usuario = user;
       this.password = password;
     }
   }
@@ -22,3 +22,7 @@ const ROUTER = document.querySelector("#ruteo");
 const HOME = document.querySelector("#pantalla-home");
 const LOGIN = document.querySelector("#pantalla-login");
 const REGISTRO = document.querySelector("#pantalla-registro");
+const LOGOUT = document.querySelector("#pantalla-logout");
+const NAV = document.querySelector("ion-nav");
+const GASTO = document.querySelector("#pantalla-gasto");
+
